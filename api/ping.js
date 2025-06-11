43149dca049fbd3eaa2301686c8f707ca588f473
Klampfe478 <f.hilbert@uke.de>
2025-06-11 14:16:59 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
   const target = "https://317797e1-89aa-42d2-a9b6-c25cd8a0c982-00-3s387r4cb4eli.riker.replit.dev";
-
   try {
     const response = await fetch(target);
     console.log("✅ Ping gesendet an Replit:", target);
